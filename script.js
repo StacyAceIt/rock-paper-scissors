23f3a8db8b347ffe6bceb00bb1906ed3d716e657
StacyAceIt <30737898+StacyAceIt@users.noreply.github.com>
2024-06-15 16:14:09 -0400

    finished all steps

--- script.js
+++ script.js
@@ -3,20 +3,50 @@ function getComputerChoice(){
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
-console.log("computer choice: " + getComputerChoice());
+
 
 function getHumanChoice(){
     let userInput = prompt("Please enter your choice (rock, paper, or scissors)").toLowerCase();
     while (!choices.includes(userInput)){
-        prompt("Invalid input. Please enter your choice (rock, paper, or scissors)").toLowerCase();
+        userInput = prompt("Invalid input. Please enter your choice (rock, paper, or scissors)").toLowerCase();
     }
     return userInput;
 }
-console.log("human choice: " + getHumanChoice());
 
-let humanScore = 0;
-let computerScore = 0;
 
-function playRound(humanChoice, computerChoice){
-    
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice){
+            console.log("Game even");
+        }else if((humanChoice === "scissors" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "rock" && computerChoice === "scissors")){
+            humanScore += 1;
+            console.log("You won!");
+            
+        }
+        else{
+            computerScore += 1;
+            console.log("You lost");        
+        }
+    }
+    for (let i = 0; i < 5; i++){
+        const humanSelection = getHumanChoice();
+        console.log("human choice: " + humanSelection);
+        const computerSelection = getComputerChoice();
+        console.log("computer choice: " + computerSelection);
+
+        playRound(humanSelection, computerSelection);
+
+    }
+    console.log("Final: " + ((humanScore > computerScore) ? "You won":
+                            (humanScore < computerScore) ? "You lost":
+                                                            "Game even"));
 }
+  
+
+playGame(); 
