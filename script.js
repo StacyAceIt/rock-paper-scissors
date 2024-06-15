ac626c439a3d565de9e403f7abb2b68ad1c18674
StacyAceIt <30737898+StacyAceIt@users.noreply.github.com>
2024-06-15 14:20:46 -0400

    finished getHumanChoice function for step3 and declare the players score for step 4

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-function getComputerChoice(){
-    const choices = ["rock", "paper", "scissors"];
+const choices = ["rock", "paper", "scissors"];
+function getComputerChoice(){   
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
-console.log(getComputerChoice());
+console.log("computer choice: " + getComputerChoice());
 
-let userInput = prompt("Please enter your choice: ", undefined);
 function getHumanChoice(){
+    let userInput = prompt("Please enter your choice (rock, paper, or scissors)").toLowerCase();
+    while (!choices.includes(userInput)){
+        prompt("Invalid input. Please enter your choice (rock, paper, or scissors)").toLowerCase();
+    }
+    return userInput;
+}
+console.log("human choice: " + getHumanChoice());
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
     
 }
-console.log(getHumanChoice());
