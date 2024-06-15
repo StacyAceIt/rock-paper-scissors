5361813da9ad0775be16b524fb92f8fe930e5d75
StacyAceIt <30737898+StacyAceIt@users.noreply.github.com>
2024-06-15 14:07:28 -0400

    added getComputerChoice function for step 2

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function getComputerChoice(){
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+console.log(getComputerChoice());
+
+let userInput = prompt("Please enter your choice: ", undefined);
+function getHumanChoice(){
     
 }
+console.log(getHumanChoice());
