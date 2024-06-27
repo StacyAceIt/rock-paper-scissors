c67eeac92ffae5f72ae68fee0e7cd10811f5f72b
StacyAceIt <30737898+StacyAceIt@users.noreply.github.com>
2024-06-27 17:11:22 -0400

    working on it

--- script.js
+++ script.js
@@ -4,52 +4,29 @@ function getComputerChoice(){
     return choices[randomIndex];
 }
 
-
-function getHumanChoice(){
-    let userInput = prompt("Please enter your choice (rock, paper, or scissors)").toLowerCase();
-    while (!choices.includes(userInput)){
-        userInput = prompt("Invalid input. Please enter your choice (rock, paper, or scissors)").toLowerCase();
-    }
-    return userInput;
-}
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice){
-        return 0;
-    }else if((humanChoice === "scissors" && computerChoice === "paper") ||
-    (humanChoice === "paper" && computerChoice === "rock") ||
-    (humanChoice === "rock" && computerChoice === "scissors")){
-        return 1;        
-    }
-    else{
-        return -1;        
-    }
-}
+const buttonClicked = document.querySelector("#buttons");
+let humanScore = 0;
+let computerScore = 0;
+buttonClicked.addEventListener('click', (event) => {
+    let output = document.querySelector("#output");
+    let humanChoice = event.target.id;
+    output.innerHTML += `You chose ${humanChoice} <br>`;
+    let computerChoice = getComputerChoice();
+    output.innerHTML += `Computer chose ${computerChoice}`;
+    output = ""
+});
 
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
+// function playRound(humanChoice, computerChoice) {
+//     if (humanChoice === computerChoice){
+//         return 0;
+//     }else if((humanChoice === "scissors" && computerChoice === "paper") ||
+//     (humanChoice === "paper" && computerChoice === "rock") ||
+//     (humanChoice === "rock" && computerChoice === "scissors")){
+//         return 1;        
+//     }
+//     else{
+//         return -1;        
+//     }
+// }
 
-    
-    for (let i = 0; i < 5; i++){
-        const humanSelection = getHumanChoice();
-        console.log("human choice: " + humanSelection);
-        const computerSelection = getComputerChoice();
-        console.log("computer choice: " + computerSelection);
-
-        let res = playRound(humanSelection, computerSelection);
-        if (res === 1){
-            humanScore++;
-        }else if(res == -1){
-            computerScore++;
-        }
-        console.log(res);
-
-    }
-    console.log("Final: " + ((humanScore > computerScore) ? "You won":
-                            (humanScore < computerScore) ? "You lost":
-                                                            "Game even"));
-}
-  
-
-playGame(); 
